b16ba9d48beaa84ac57fcfa6859c9281742e5916
Hendrik Abdalla Hermann <48130931+HendrikAH12@users.noreply.github.com>
2022-02-16 15:15:00 -0300

    .

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,11 +5,9 @@
 
 <script>
   import Navbar from './components/Navbar.vue'; 
-  import FormPromocoes from "./components/FormPromocoes"; 
   export default {
     components: {
-      Navbar,
-      FormPromocoes
+      Navbar
     }
   }
 </script>
--- frontend/src/views/CriaPromocoes.vue
+++ frontend/src/views/CriaPromocoes.vue
@@ -1,5 +1,3 @@
-
-
 <template>
-    <FormPromocoes />
+    
 </template>
