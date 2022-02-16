f265110a0ada3a8c6a9c1804c046e8348b226ba7
Hendrik Abdalla Hermann <48130931+HendrikAH12@users.noreply.github.com>
2022-02-16 15:25:30 -0300

    .

--- frontend/src/views/service.js
+++ frontend/src/views/service.js
@@ -2,4 +2,8 @@ export default class ProductService {
 	getProducts() {
 		return fetch('demo/content.json').then(res => res.json()).then(d => d.data);
     }
+	
+	getProductsAlteracao() {
+		return fetch('demo/data/products.json').then(res => res.json()).then(d => d.data);
+    }
 }
