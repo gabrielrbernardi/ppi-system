f28198795ad25d2639842f3f003d367f61f08ca0
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2022-02-21 18:05:42 -0300

    .

--- frontend/src/views/service.js
+++ frontend/src/views/service.js
@@ -1,10 +1,11 @@
 export default class ProductService {
 	getProducts() {
+		// get do axios futuramente
 		return fetch('demo/content.json').then(res => res.json()).then(d => d.data);
     }
 	
 	async getProductsAlteracao() {
-		console.log(1);
+		// Get do axios futuramente
 		return fetch('demo/product.json').then(res => res.json()).then(d => d.data); 
 
     }
