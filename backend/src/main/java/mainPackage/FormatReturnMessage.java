ae1e94034c6ac482fee5dc2c8c74b3223a991df9
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2022-03-28 12:54:54 -0300

    crud finished. update controller

--- backend/src/main/java/mainPackage/FormatReturnMessage.java
+++ backend/src/main/java/mainPackage/FormatReturnMessage.java
@@ -1,19 +1,30 @@
 package mainPackage;
 
+import org.springframework.http.HttpStatus;
+
 public class FormatReturnMessage {
 	private String text;
+	private HttpStatus response;
 
-	public FormatReturnMessage(String text) {
+	public FormatReturnMessage(String text, HttpStatus response) {
 		super();
 		this.text = text;
+		this.response = response;
 	}
-
+	
 	public String getText() {
 		return text;
 	}
 
 	public void setText(String text) {
 		this.text = text;
+	}
+	public HttpStatus getResponse() {
+		return response;
+	}
+	
+	public void setResponse(HttpStatus response) {
+		this.response = response;
 	}
 	
 	
--- backend/src/main/java/mainPackage/ProdutoController.java
+++ backend/src/main/java/mainPackage/ProdutoController.java
@@ -1,11 +1,14 @@
 package mainPackage;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,19 +21,62 @@ public class ProdutoController {
 	@Autowired
 	private ProductRepository productRepository;
 	
-	@GetMapping(path="/")
-	public @ResponseBody String status() {
-		return "ola";
+	@GetMapping(path="/status")
+	public @ResponseBody FormatReturnMessage status() {
+		return new FormatReturnMessage("true", HttpStatus.OK);
 	}
 	
-	@PostMapping(value="/add", produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value="/")
 	@ResponseBody
-	public FormatReturnMessage addProduct (@Valid @RequestBody ProdutoDomain prod) {
-		ProdutoDomain p = new ProdutoDomain();
-		p.setCodProduto(prod.getCodProduto());
-		p.setNomeProduto(prod.getNomeProduto());
-		productRepository.save(p);
+	public ResponseEntity<ProdutoDomain> addProduct (@Valid @RequestBody ProdutoDomain prod) {
+		ProdutoDomain pd = new ProdutoDomain();
+		pd.setCodProduto(prod.getCodProduto());
+		pd.setNomeProduto(prod.getNomeProduto());
+		productRepository.save(pd);
 			
-		return new FormatReturnMessage("ALOU");
+		return new ResponseEntity<>(pd, HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{id}")
+	@ResponseBody
+	public ResponseEntity<ProdutoDomain> getSpecificProduct(@PathVariable Long id){
+		try {
+			ProdutoDomain pd = productRepository.findById(id).orElseThrow(() -> new Exception("Produto nao encontrado"));
+			return new ResponseEntity<>(pd, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@GetMapping(value="/getAll")
+	@ResponseBody
+	public Iterable<ProdutoDomain> getAllProducts() {
+		return productRepository.findAll();
+	}
+	
+	@PutMapping(value="/{id}")
+	@ResponseBody
+	public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody ProdutoDomain product) throws Exception{
+		try {
+			ProdutoDomain pd = productRepository.findById(id).orElseThrow(() -> new Exception("Produto nao encontrado")); //retornara o objeto se for encontrado ou vazio, caso contrario
+			pd.setNomeProduto(product.getNomeProduto());
+			pd.setCodProduto(product.getCodProduto());
+			
+			productRepository.save(pd);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>("Produto nao encontrado", HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping(value="/{id}")
+	@ResponseBody
+	public ResponseEntity<String> deleteProduct(@PathVariable Long id){
+		try {
+			productRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>("Erro na execucao", HttpStatus.NOT_FOUND);
+		}
 	}
 }
